a95e3f99b6b4acc6b09468aabec3358829f9d7de
Justin Hinds <jth140@scarletmail.rutgers.edu>
2019-11-20 13:48:16 -0500

    Lab 8 Submission

---
+++ lab8code/myapp.c
@@ -0,0 +1,25 @@
+/*
+ * Placeholder PetaLinux user application.
+ *
+ * Replace this with your application code
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	char *welcome;
+#ifdef WELCOME
+	welcome=WELCOME;
+#else
+	welcome="Petalinux World!";
+#endif
+	printf("Hello, %s\n", welcome);
+	printf("cmdline args:\n");
+	while(argc--)
+		printf("%s\n",*argv++);
+
+	return 0;
+}
+
+
